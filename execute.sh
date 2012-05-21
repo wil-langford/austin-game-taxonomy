57a29b5679ddb6cba93418e2220c0fe6f543b728
Wil Langford <wsl@pdx.edu>
2012-05-20 22:19:57 -0700

    Generalized paths.  No complaints from the MPI runtime.

--- execute.sh
+++ execute.sh
@@ -2,5 +2,5 @@
 #PBS -l nodes=3
 source /opt/torque/etc/openmpi-setup.sh
 
-mpirun /home/wsl/git/austin-game-taxonomy/virtual-python/bin/python /home/wsl/git/austin-game-taxonomy/game-taxonomy/python-mpi-skeleton.py
+mpirun $HOME/bin/python $HOME/austin-game-taxonomy/python-mpi-skeleton.py
 
