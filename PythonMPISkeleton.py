f9640c80ce092cedb482f145a58a915f43f00881
Wil Langford <wsl@pdx.edu>
2012-05-20 23:18:40 -0700

    Point-to-point communication between master and minions.

--- PythonMPISkeleton.py
+++ PythonMPISkeleton.py
@@ -1,4 +1,9 @@
 #!/home/wsl/virtual-python/bin/python
+"""
+This is a skeleton/example Python MPI program.
+
+- Wil Langford
+"""
 
 import numpy as np
 from mpi4py import MPI
@@ -12,10 +17,26 @@ if rank == 0:
     print "master"
     BASEDIR = os.path.expanduser('~/austin-game-taxonomy')
     DATADIR = BASEDIR + '/data'
+    MASTERFILE = DATADIR + '/master.dat'
 
-    with open(DATADIR + "/master.dat", 'w') as f:
-        f.write("my rank is {0}".format(rank))
+    size = comm.Get_size()
+
+    with open(MASTERFILE, 'a') as f:
+        f.write("I am Process ID {0}.\nMy MPI rank is {1}\n".format(os.getpid(), rank))
+
+        for minionrank in range(1, size):
+            S = np.empty([2, 2])
+            S.fill(minionrank)
+            R = np.zeros([2, 2])
+            
+            comm.Send(S, dest=minionrank, tag=minionrank)
+            comm.Recv(R, source=minionrank, tag=minionrank)
+            f.write("To minion {0} I sent: {1}\nMinion {0} replied with: {2}\n".format(minionrank, S[0,0], R[0,0]))
 
 else:
     # I am a minion node!
     print "worker {0}".format(rank)
+
+    R = np.zeros([2, 2])
+    comm.Recv(R, source=0, tag=rank)
+    comm.Send(-1*R, dest=0, tag=rank)
