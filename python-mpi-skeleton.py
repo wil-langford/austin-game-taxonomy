bd5daecec3b23785718d7c37d51eaeca0752db94
Wil Langford <wsl@pdx.edu>
2012-05-20 22:20:17 -0700

    Generalized paths.

--- python-mpi-skeleton.py
+++ python-mpi-skeleton.py
@@ -1,8 +1,8 @@
 #!/home/wsl/virtual-python/bin/python
 
-
 import numpy as np
 from mpi4py import MPI
+import os
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -10,8 +10,9 @@ rank = comm.Get_rank()
 if rank == 0:
     # All shall bow before me, for I am the master node!
     print "master"
-    BASEDIR = '/home/wsl/git/austin-game-taxonomy/game-taxonomy'
+    BASEDIR = os.path.expanduser('~/austin-game-taxonomy')
     DATADIR = BASEDIR + '/data'
+
     with open(DATADIR + "/master", 'w') as f:
         f.write("my rank is {0}".format(rank))
 
